5c339eac098ece1186f2eb147bf9539f1b4e1150
Tony Li <tonyxli20@gmail.com>
2022-04-21 10:16:29 -0700

    updated single product

--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,19 +1,6 @@
 import axios from 'axios';
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 
-<<<<<<< HEAD
-export const getSingleProduct = createAsyncThunk('product/getProduct', async (id) => {
-    const {data} = await axios.get(`/api/products/${id}`);
-    return data
-  })
-
-export const singleProductSlice = createSlice({
-  name: 'product',
-  initialState: {
-    product: {},
-    status: null
-  },
-=======
 const initialState = {
   product: {},
   status: null,
@@ -33,20 +20,11 @@ export const getSingleProduct = createAsyncThunk('product/getProduct', async (id
   export const singleProductSlice = createSlice({
   name: 'product',
   initialState,
->>>>>>> c565621e83830ea66f3ca588393c991a43f0b006
   reducers: {},
   extraReducers: {
     [getSingleProduct.pending]: (state) => {
       state.status = 'loading'
     },
-<<<<<<< HEAD
-    [getSingleProduct.fulfilled]: (state, {payload}) => {
-      state.value = payload;
-      state.status = 'success';
-    },
-    [getSingleProduct.rejected]: (state, action) => {
-      state.status = 'failed';
-=======
     [getSingleProduct.fulfilled]: (state, action) => {
       state.product = action.payload;
       state.status = 'success';
@@ -54,20 +32,11 @@ export const getSingleProduct = createAsyncThunk('product/getProduct', async (id
     [getSingleProduct.rejected]: (state, action) => {
       state.status = 'rejected';
       state.error = action.payload
->>>>>>> c565621e83830ea66f3ca588393c991a43f0b006
     }
   }
 })
 
-<<<<<<< HEAD
-const singleProductReducer = singleProductSlice.reducer;
-
-export default singleProductReducer;
-
-// No need to return dispatch function, just data
-=======
 // const singleProductReducer = singleProductSlice.reducer;
 // export default singleProductReducer;
 
 export const singleProductReducer = singleProductSlice.reducer;
->>>>>>> c565621e83830ea66f3ca588393c991a43f0b006
