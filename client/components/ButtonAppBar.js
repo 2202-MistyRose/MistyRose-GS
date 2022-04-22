ea45fc17f495987138b4e9029e5334fab652833d
brandon-man <nybrandonman@gmail.com>
2022-04-22 14:21:50 -0700

    Fixed routes for Login and Signup

--- client/components/ButtonAppBar.js
+++ client/components/ButtonAppBar.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from 'react-router-dom';
-import { styled } from '@material-ui/core/styles';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "react-router-dom";
+import { logout } from "../store/auth.slice";
+import { styled } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,6 +26,9 @@ const useStyles = makeStyles((theme) => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+  const { success } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
@@ -31,11 +36,11 @@ export default function ButtonAppBar() {
           <Link to="/">
             <img
               style={{
-                height: '2.5em',
-                width: '1.8em',
+                height: "2.5em",
+                width: "1.8em",
 
-                marginLeft: '2em',
-                marginRight: '13em',
+                marginLeft: "2em",
+                marginRight: "13em",
               }}
               src="/images/pearpng.png"
             />
@@ -71,7 +76,23 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Accessories
           </Typography>
-          <Button color="inherit">Login</Button>
+          {/* <Button color="inherit">Login</Button> */}
+          {success ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <Link to="/products">Products</Link>
+              <a href="#" onClick={() => dispatch(logout())}>
+                Logout
+              </a>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
