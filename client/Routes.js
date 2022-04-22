26c420fcfaa737cfef2970cc35ff30d00c7a6f80
demetrius robinson <demetrius.robinson@icloud.com>
2022-04-22 16:23:09 -0400

    nav

--- client/Routes.js
+++ client/Routes.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import React, { useEffect } from 'react';
-import { connect, useDispatch, useSelector } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Login from './components/Login';
+import { useDispatch } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import SignUp from './components/SignUp';
-=======
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch, Redirect } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import { me } from "./store/auth.slice";
-import AllProducts from "./components/AllProducts";
->>>>>>> bfd88f9858da6bbc53ba3a2facccad286f72667e
+import Login from './components/Login';
+import Home from './components/Home';
+import { me } from './store/auth.slice';
+import AllProducts from './components/AllProducts';
 
 import Home from './components/Home';
 import { me } from './store';
