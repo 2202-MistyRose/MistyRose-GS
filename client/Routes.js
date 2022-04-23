9cc230621c268ad859f7754c841269469a05f9f0
Tony Li <tonyxli20@gmail.com>
2022-04-23 10:31:48 -0700

    filler branch

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
 import AllProducts from "./components/AllProducts";
+import SingleProduct from "./components/SingleProduct";
 import Cart from "./components/Cart";
 import { me } from "./store/auth.slice";
 /**
@@ -21,7 +22,8 @@ const Routes = () => {
     <div>
       <Switch>
         <Route path="/home" component={Home} />
-        <Route path="/products" component={AllProducts} />
+        <Route exact path="/products" component={AllProducts} />
+        <Route path="/products/:productId" component={SingleProduct}/>
         <Route path="/users/:userId/cart" component={Cart} />
         <Route path="/login">{Login}</Route>
         <Route path="/signup">{Signup}</Route>
