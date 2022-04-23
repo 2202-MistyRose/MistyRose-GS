6b76c2a779c7acc851cca9877659e4c56f60ce8c
demetrius robinson <demetrius.robinson@icloud.com>
2022-04-23 13:33:59 -0400

    got hero image in place

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -16,6 +16,7 @@ const Hero = () => {
     // </>
     <div className="hero-banner-container">
       <div>
+        <h1>Our #1 product!!</h1>
         <img
           className="hero-banner-image"
           src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
